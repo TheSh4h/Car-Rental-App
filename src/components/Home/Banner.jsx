fe88bf2c561b44e8752e66cd6aa90851b51c27ff
TheSh4h <hh.haseebshah@gmail.com>
2023-10-19 08:16:13 -0700

    banner created

---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+    return ( 
+        <div className="text-center bg-black">
+            <div className="p-10">
+                <h1 className="text-4xl text-white font-bold mb-1">Save big with our cheap car rental!</h1>
+                <p className="text-white text-2xl">Top Airports. Local Suppliers. 24/7 Support.</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,16 @@
 import Hero from "./Hero";
 import Booking from "./Booking";
 import Plan from "../Plan";
+import Banner from "./Banner";
 import Testimonials from "./Testimonials";
 
 const Home = () => {
     return ( 
-        <div className="container mx-auto">
+        <div>
             <Hero />
             <Booking />
             <Plan />
+            <Banner />
             <Testimonials />
         </div>
      );
