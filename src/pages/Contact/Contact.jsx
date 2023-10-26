da7fbb3a2021f907b70d47e389b77dcb1023a51f
TheSh4h <hh.haseebshah@gmail.com>
2023-10-26 11:15:32 -0700

    made responsive

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,13 +3,13 @@ import CallBanner from "../../components/CallBanner";
 const Contact = () => {
     return ( 
         <div className="bg-white text-center">
-            <div className="container mx-auto px-10 py-28 flex justify-between">
-                <div className="text-start w-96">
+            <div className="container mx-auto px-10 py-28 lg:flex justify-between">
+                <div className="text-start lg:w-96">
                     <h1 className="text-5xl font-bold">Need additional information?</h1>
                     <p className="py-8 text-slate-500">A multifaceted professional skilled in multiple fields of research, development as well as a learning specialist. Over 15 years of experience.</p>
                     <ul >
                         <li className="font-bold">
-                            <a href="tel:123456789" className="flex justify-center md:justify-start">
+                            <a href="tel:123456789" className="flex justify-start">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                                 </svg>
@@ -17,7 +17,7 @@ const Contact = () => {
                             </a>
                         </li>
                         <li className="py-5">
-                            <a href="mailto:car@rental.com" className="flex justify-center md:justify-start">
+                            <a href="mailto:car@rental.com" className="flex justify-start">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                                 </svg>
@@ -27,7 +27,7 @@ const Contact = () => {
                     </ul>
                 </div>
 
-                <div className="text-start w-[600px]">
+                <div className="text-start md:w-[600px]">
                     <label htmlFor="name" className="font-semibold">Full Name:</label> <br />
                     <input id="name" className="bg-slate-200 w-full p-3 my-3 shadow-lg rounded-sm" type="text" placeholder="eg. John Shamoe" />
                     <br />
