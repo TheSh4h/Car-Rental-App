c835badbe1009e058642ab30d2ddcc7bc08acce0
TheSh4h <hh.haseebshah@gmail.com>
2023-10-30 05:07:24 -0700

    vehicle page done

--- src/pages/Vehicles/Vehicles.jsx
+++ src/pages/Vehicles/Vehicles.jsx
@@ -1,17 +1,19 @@
 import CarCard from "../../components/CarCard";
+import CallBanner from "../../components/CallBanner"
 import cars from "../../data/car";
 
 const Vehicles = () => {
     
     return ( 
-        <div className="flex justify-center items-center text-center">
-            <div className="container mx-28 py-20 grid grid-cols-3 gap-8">
+        <div className="">
+            <div className="m-28 grid grid-cols-3 gap-8">
                 {
                     cars.map((car) => {
                         return <CarCard car={car} />
                     })
                 }
             </div>
+            <CallBanner />
         </div>
      );
 }
