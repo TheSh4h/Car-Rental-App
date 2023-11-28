9ec3bb58a79e2e661084c2834d9e2021c6e43dbd
TheSh4h <hh.haseebshah@gmail.com>
2023-11-28 20:11:28 +0500

    fixed banner

--- src/pages/Vehicles/Vehicles.jsx
+++ src/pages/Vehicles/Vehicles.jsx
@@ -5,13 +5,15 @@ import cars from "../../data/car";
 const Vehicles = () => {
     
     return ( 
-        <div className="flex flex-col justify-center items-center my-28">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {
-                    cars.map((car) => {
-                        return <CarCard car={car} />
-                    })
-                }
+        <div> 
+            <div className="flex flex-col justify-center items-center my-28">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {
+                        cars.map((car) => {
+                            return <CarCard car={car} />
+                        })
+                    }
+                </div>
             </div>
             <CallBanner />
         </div>
