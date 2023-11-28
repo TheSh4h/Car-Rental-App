ee06def752c65f66966e0277e1e8d685f81ac434
TheSh4h <hh.haseebshah@gmail.com>
2023-11-28 20:10:04 +0500

    grid fixed

--- src/pages/Vehicles/Vehicles.jsx
+++ src/pages/Vehicles/Vehicles.jsx
@@ -5,8 +5,8 @@ import cars from "../../data/car";
 const Vehicles = () => {
     
     return ( 
-        <div className="">
-            <div className="m-28 grid grid-cols-3 gap-8">
+        <div className="flex flex-col justify-center items-center my-28">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {
                     cars.map((car) => {
                         return <CarCard car={car} />
